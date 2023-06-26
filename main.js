c5b40abac5859b0d130449d222f510d8508db5e5
aebystonks <grimesjohnnn@gmail.com>
2023-06-27 02:38:45 +0600

    changed script

--- main.js
+++ main.js
@@ -1,13 +1,12 @@
 for (let i = 0; i < 13; i++) {
-  if(i % 2 == 0) continue;
-	console.log(`Нечетное число: ${i}`)
+  if(i % 2 == 0) {
+		console.log(`Четное число: ${i}`);
+		continue;
+	}
+	else if(i % 2 == 1){
+		console.log(`Нечетное число: ${i}`);
+		continue;
+	}
 }
 
-console.log('\n')
 
-for(let j = 0; j < 13; j++){
-	if(j % 2 == 1) continue;
-	console.log(`Четное число: ${j}`)
-}
-
-console.log("Всего четных чисел: 7 \n Нечетных чисел: 6") // Чисто 0 зарешал )
